ecf2b76381b3501dc3678238eae3ab6aa9c350dd
樊玉顺 <fooww-fanyushun@example.com>
2019-06-03 20:43:41 +0800

    添加 checkbox 的学习笔记

--- packages/checkbox/src/checkbox.vue
+++ packages/checkbox/src/checkbox.vue
@@ -77,19 +77,19 @@
       return {
         selfModel: false,
         focus: false,
-        isLimitExceeded: false
+        isLimitExceeded: false // 限制是否可以选中，在数量的限制时使用
       };
     },
 
     computed: {
       model: {
-        get() {
+        get() { // 首先判断其父组件是否是 checkboxGroup ，如果是的话返回 checkboxGroup 的 value ，如果不是的话判断当前组件的 value 是否是 undefined ，否的话返回当前组件的 value ，否则返回 false
           return this.isGroup
             ? this.store : this.value !== undefined
               ? this.value : this.selfModel;
         },
 
-        set(val) {
+        set(val) { // 需要先判断其父组件是否是 checkboxGroup
           if (this.isGroup) {
             this.isLimitExceeded = false;
             (this._checkboxGroup.min !== undefined &&
@@ -99,7 +99,7 @@
             (this._checkboxGroup.max !== undefined &&
               val.length > this._checkboxGroup.max &&
               (this.isLimitExceeded = true));
-
+            // 当存在最小/最大个数的限制时，当前选中个数在最小和最大数之间的时候才会触发 checkboxGroup 的 input 事件
             this.isLimitExceeded === false &&
             this.dispatch('ElCheckboxGroup', 'input', [val]);
           } else {
@@ -109,17 +109,17 @@
         }
       },
 
-      isChecked() {
-        if ({}.toString.call(this.model) === '[object Boolean]') {
+      isChecked() { // 判断是否选中
+        if ({}.toString.call(this.model) === '[object Boolean]') { // 如果是 true/false ，直接返回 true/false
           return this.model;
-        } else if (Array.isArray(this.model)) {
+        } else if (Array.isArray(this.model)) { // 如果是 数组 ，查看数组中是否包含当前 label
           return this.model.indexOf(this.label) > -1;
-        } else if (this.model !== null && this.model !== undefined) {
+        } else if (this.model !== null && this.model !== undefined) { // 如果既不是 null 也不是 undefined ，则和指定的 trueLabel 比较
           return this.model === this.trueLabel;
         }
       },
 
-      isGroup() {
+      isGroup() { // 判断其父组件是否是 checkboxGroup
         let parent = this.$parent;
         while (parent) {
           if (parent.$options.componentName !== 'ElCheckboxGroup') {
@@ -132,21 +132,21 @@
         return false;
       },
 
-      store() {
+      store() { // 父组件 checkboxGroup 的 value
         return this._checkboxGroup ? this._checkboxGroup.value : this.value;
       },
 
-      isDisabled() {
+      isDisabled() { // 首先判断其父组件是否是 checkboxGroup ，如果是的话 disabled 优先级：checkboxGroup.disabled >  this.disabled > this.elForm.disabled，否则：this.disabled > this.elForm.disabled
         return this.isGroup
           ? this._checkboxGroup.disabled || this.disabled || (this.elForm || {}).disabled
           : this.disabled || (this.elForm || {}).disabled;
       },
 
-      _elFormItemSize() {
+      _elFormItemSize() { // 返回 formItem 的 size
         return (this.elFormItem || {}).elFormItemSize;
       },
 
-      checkboxSize() {
+      checkboxSize() { // 返回应用于 checkbox 的 size
         const temCheckboxSize = this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
         return this.isGroup
           ? this._checkboxGroup.checkboxGroupSize || temCheckboxSize
@@ -163,14 +163,14 @@
       name: String,
       trueLabel: [String, Number],
       falseLabel: [String, Number],
-      id: String, /* 当indeterminate为真时，为controls提供相关连的checkbox的id，表明元素间的控制关系*/
-      controls: String, /* 当indeterminate为真时，为controls提供相关连的checkbox的id，表明元素间的控制关系*/
+      id: String, /* 当 indeterminate 为真时，为 controls 提供相关连的 checkbox 的 id ，表明元素间的控制关系*/
+      controls: String,
       border: Boolean,
       size: String
     },
 
     methods: {
-      addToStore() {
+      addToStore() { // 添加当前组件的值
         if (
           Array.isArray(this.model) &&
           this.model.indexOf(this.label) === -1
@@ -180,7 +180,7 @@
           this.model = this.trueLabel || true;
         }
       },
-      handleChange(ev) {
+      handleChange(ev) { 
         if (this.isLimitExceeded) return;
         let value;
         if (ev.target.checked) {
@@ -198,7 +198,7 @@
     },
 
     created() {
-      this.checked && this.addToStore();
+      this.checked && this.addToStore(); // 当前组件选中的时候，将其添加到 mode 中
     },
     mounted() { // 为indeterminate元素 添加aria-controls 属性
       if (this.indeterminate) {
